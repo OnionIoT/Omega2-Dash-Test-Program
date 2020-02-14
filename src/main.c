61ead9584a3ebae7171a895af9237254e990cd5c
Lazar Demin <lazar@onion.io>
2020-02-14 19:10:20 +0000

    now have 5 buttons that change text and colour when pressed

--- src/main.c
+++ src/main.c
@@ -8,15 +8,52 @@
 
 #define DISP_BUF_SIZE (80*LV_HOR_RES_MAX)
 
+
+void set_button_style(lv_obj_t * btn) {
+  
+
+  
+  // set the pressed style
+  static lv_style_t style_btn_pr;
+  lv_style_copy(&style_btn_pr, &lv_style_btn_pr);         /*Initialize from a built-in style*/
+  style_btn_pr.body.main_color = lv_color_hex(0x27ae60);
+  // style_btn_pr.body.grad_color = lv_color_hex(0xecf0f1);
+  
+  lv_btn_set_style(btn, LV_BTN_STYLE_PR, &style_btn_pr);    /*Set the button's released style*/
+  
+  // set the normal style
+  static lv_style_t style_btn_rel;                        /*A variable to store the released style*/
+  lv_style_copy(&style_btn_rel, &lv_style_btn_rel);         /*Initialize from a built-in style*/
+  style_btn_rel.body.main_color = lv_color_hex(0x2ecc71);
+  style_btn_rel.body.grad_color = lv_color_hex(0xecf0f1);
+  
+  lv_btn_set_style(btn, LV_BTN_STYLE_REL, &style_btn_rel);    /*Set the button's released style*/
+}
+
 // defining an event handler for example button presses
 static void event_handler(lv_obj_t * obj, lv_event_t event)
 {
-  if(event == LV_EVENT_CLICKED) {
+  if(event == LV_EVENT_PRESSED) {
     printf("Clicked\n");
+    lv_obj_t * label = lv_obj_get_child(obj, NULL);
+    lv_label_set_text(label, "OK");
+    set_button_style(obj);
   }
-  else if(event == LV_EVENT_VALUE_CHANGED) {
-    printf("Toggled\n");
-  }
+  // else if(event == LV_EVENT_VALUE_CHANGED) {
+  //   printf("Toggled\n");
+  // }
+}
+
+// function to create buttons
+void create_button(uint8_t alignment) {
+  lv_obj_t * btnLabel;
+
+  lv_obj_t * btn1 = lv_btn_create(lv_scr_act(), NULL);
+  lv_obj_set_event_cb(btn1, event_handler);
+  lv_obj_align(btn1, NULL, alignment, 0, 0);
+
+  btnLabel = lv_label_create(btn1, NULL);
+  lv_label_set_text(btnLabel, "Press Me");
 }
 
 // main program for lv_example
@@ -50,21 +87,14 @@ int main(int argc, char *argv[]) {
   indev_drv.read_cb = xpt7603_read;      /*Set your driver function*/
   lv_indev_drv_register(&indev_drv);         /*Finally register the driver*/
   
-  /* Demo Code - replace this with yours */
-  /*Create a "Hello world!" label*/
-  lv_obj_t * label = lv_label_create(lv_scr_act(), NULL);
-  lv_label_set_text(label, "Hello world!");
-  lv_obj_align(label, NULL, LV_ALIGN_IN_TOP_MID, 0, 0);
+  /*Create 5 buttons*/
+  create_button(LV_ALIGN_IN_TOP_LEFT);
+  create_button(LV_ALIGN_IN_TOP_RIGHT);
+  create_button(LV_ALIGN_IN_BOTTOM_LEFT);
+  create_button(LV_ALIGN_IN_BOTTOM_RIGHT);
+  create_button(LV_ALIGN_CENTER);
   
-  /*Create a button*/
-  lv_obj_t * btnLabel;
-
-  lv_obj_t * btn1 = lv_btn_create(lv_scr_act(), NULL);
-  lv_obj_set_event_cb(btn1, event_handler);
-  lv_obj_align(btn1, NULL, LV_ALIGN_CENTER, 0, 0);
-
-  btnLabel = lv_label_create(btn1, NULL);
-  lv_label_set_text(btnLabel, "Press Me");
+  
   
   printf("starting loop\n");
   while(1) {
